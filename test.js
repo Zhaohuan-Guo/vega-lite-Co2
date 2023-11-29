006be82167d113e5ade4ff159b9ca8106378481d
Zhaohuan-Guo <zhaohuan.guo@ucdconnect.ie>
2023-11-29 20:33:23 +0000

    1

--- test.js
+++ test.js
@@ -16,9 +16,9 @@ function processData(content) {
   const data = rows.map((row) => row.split(",")); // 按逗号分割列数据
 
 
-  data[0][6] = "country_abbr";
+  data[0][5] = "country_abbr";
   for (let i = 1; i < data.length; i++) {
-    data[i][6] = countryList.getCode(data[i][0]);
+    data[i][5] = countryList.getCode(data[i][0]);
   }
 
   const processedData = data.map((row) => row.join(",")); // 将数据转换回CSV格式的行
