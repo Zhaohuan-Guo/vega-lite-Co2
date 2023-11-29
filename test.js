4fcb1d700bc0e15fc84336c077b44e0fccfe0e35
Zhaohuan-Guo <zhaohuan.guo@ucdconnect.ie>
2023-11-29 20:31:43 +0000

    1

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const countryList = require("country-list");
 
 // 读取CSV文件
 fs.readFile("updated_data.csv", "utf8", (err, data) => {
@@ -14,12 +15,11 @@ function processData(content) {
   const rows = content.split("\n"); // 按行分割
   const data = rows.map((row) => row.split(",")); // 按逗号分割列数据
 
-  let dict = {};
-  for (let i = 11; i < data.length; i+=11) {
-    data[i][6] = data[i-10][6]
-    data[i-10][6] = "0"
-  }
 
+  data[0][6] = "country_abbr";
+  for (let i = 1; i < data.length; i++) {
+    data[i][6] = countryList.getCode(data[i][0]);
+  }
 
   const processedData = data.map((row) => row.join(",")); // 将数据转换回CSV格式的行
 
--- test1.js
+++ test1.js
@@ -1,1 +1,7 @@
-console.log('co2_emission_non_animals\r')
+const countryList = require('country-list');
+
+// 将国家名称转换为国家缩写
+const countryName = 'United States of America';
+const countryCode = countryList.getCode(countryName);
+
+console.log(countryCode); // 输出 "US"
