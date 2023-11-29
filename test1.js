c9462568ac59abfd31600053bd8f1405fd16a6c3
Zhaohuan-Guo <zhaohuan.guo@ucdconnect.ie>
2023-11-29 20:53:30 +0000

    1

--- test1.js
+++ test1.js
@@ -1,7 +1,7 @@
 const countryList = require('country-list');
 
 // 将国家名称转换为国家缩写
-const countryName = 'Hong Kong';
+const countryName = 'Russia';
 const countryCode = countryList.getCode(countryName);
 
 console.log(countryCode); // 输出 "US"
